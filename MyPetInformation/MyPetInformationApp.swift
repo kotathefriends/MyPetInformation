ca1fd2d1721fbebed2c22dd0ada094a864dcadf2
kotathefriends <5070kouta@gmail.com>
2023-10-17 15:32:14 +0900

    Initial Commit

---
+++ MyPetInformation/MyPetInformationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPetInformationApp.swift
+//  MyPetInformation
+//
+//  Created by KOTA TAKAHASHI on 2023/10/17.
+//
+
+import SwiftUI
+
+@main
+struct MyPetInformationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
